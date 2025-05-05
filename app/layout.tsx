ee17b8ff88224348ac8e5d5cd20e7a18dbbbec66
Bankole Ona ara <richwaydev01@gmail.com>
2025-05-05 03:03:58 -0700

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,32 @@
 import type { Metadata } from 'next'
+import Head from 'next/head';
 import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Honor Electric',
-  description: 'Elctrician',
-  generator: 'electrical',
-}
+  description: 'Electrician services with professionalism and expertise.',
+  openGraph: {
+    title: 'Honor Electric',
+    description: 'Professional electrical services for homes and businesses.',
+    url: 'https://HonorElectric.vercel.app',
+    siteName: 'Honor Electric',
+    images: [
+      {
+        url: 'https://HonorElectric.vercel.app/image-removebg-preview.png', 
+        width: 1200,
+        height: 630,
+        alt: 'Honor Electric Preview Image',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Honor Electric',
+    description: 'Professional electrical services.',
+    images: ['https://HonorElectric.vercel.app/image-removebg-preview.png'],
+  },
+};
 
 export default function RootLayout({
   children,
@@ -14,6 +35,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      
       <body>{children}</body>
     </html>
   )
